ae8f1da008a749cae156d9c3b8c1979bf09da47c
CyberMuffin <cybermuffinofficial@gmail.com>
2023-02-15 11:33:47 +0530

    Update program.cs

--- InvoiceApp/Program.cs
+++ InvoiceApp/Program.cs
@@ -1,9 +1,17 @@
+using InvoiceApp.Application;
+using InvoiceApp.Domain.Interfaces;
+using InvoiceApp.Domain.Services;
+using InvoiceApp.Infrastructure.DataAccess;
+using MediatR;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSingleton<ICosmosConnection>(await InitializeCosmosConnectionAsync(builder.Configuration.GetSection("CosmosDb")));
+builder.Services.AddApplication();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,3 +31,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+async Task<ICosmosConnection> InitializeCosmosConnectionAsync(IConfigurationSection configurationSection)
+{
+    var databaseName = configurationSection["DatabaseId"];
+    var containerName = configurationSection["ContainerId"];
+    var endpointUri = configurationSection["EndpointUri"];
+    var primaryKey = configurationSection["PrimaryKey"];
+    var client = new Microsoft.Azure.Cosmos.CosmosClient(endpointUri, primaryKey);
+    var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+    await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id", 500);
+    var cosmosConnection = new CosmosConnection(client, databaseName, containerName);
+    return cosmosConnection;
+}
