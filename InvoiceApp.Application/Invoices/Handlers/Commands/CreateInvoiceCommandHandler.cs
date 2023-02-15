d158a69848002041a62e704300f7bb54954c9a33
CyberMuffin <cybermuffinofficial@gmail.com>
2023-02-15 11:31:37 +0530

    Create command handlers

---
+++ InvoiceApp.Application/Invoices/Handlers/Commands/CreateInvoiceCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using InvoiceApp.Application.Invoices.Commands;
+using InvoiceApp.Domain.Aggregates.InvoiceAggregate;
+using InvoiceApp.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Application.Invoices.Handlers.Commands;
+
+public class CreateInvoiceCommandHandler : IRequestHandler<CreateInvoiceCommand, Invoice>
+{
+    private IInvoiceService _invoiceService;
+
+    public CreateInvoiceCommandHandler(IInvoiceService invoiceService)
+    {
+        _invoiceService = invoiceService;
+    }
+
+    public async Task<Invoice> Handle(CreateInvoiceCommand request, CancellationToken cancellationToken)
+    {
+        await _invoiceService.CreateAsync(request.Invoice);
+        return request.Invoice;
+    }
+}
---
+++ InvoiceApp.Application/Invoices/Handlers/Commands/DeleteInvoiceCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using InvoiceApp.Application.Invoices.Commands;
+using InvoiceApp.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Application.Invoices.Handlers.Commands;
+
+public class DeleteInvoiceCommandHandler : IRequestHandler<DeleteInvoiceCommand>
+{
+
+    private IInvoiceService _invoiceService;
+
+    public DeleteInvoiceCommandHandler(IInvoiceService invoiceService)
+    {
+        _invoiceService = invoiceService;
+    }
+
+    public async Task<Unit> Handle(DeleteInvoiceCommand request, CancellationToken cancellationToken)
+    {
+        await _invoiceService.DeleteAsync(request.Id);
+        return Unit.Value;
+    }
+}
